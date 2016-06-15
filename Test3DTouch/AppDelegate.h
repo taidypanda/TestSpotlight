1c3fcc3884a30c41099d6c53c1bdf9588f1eec22
taidy <xiongguob@163.com>
2016-06-15 10:27:37 +0800

    Initial Commit

---
+++ Test3DTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test3DTouch
+//
+//  Created by 苹果 on 16/6/15.
+//  Copyright © 2016年 苹果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test3DTouch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test3DTouch
+//
+//  Created by 苹果 on 16/6/15.
+//  Copyright © 2016年 苹果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
